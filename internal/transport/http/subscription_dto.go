246bf3c50ae56c9d74cfd6891e6f2f5482d1609f
KEI3AB <ramazanoff06@yandex.ru>
2026-05-31 14:55:25 +0300

    feat: implement DTO

---
+++ internal/transport/http/subscription_dto.go
@@ -0,0 +1,54 @@
+package http
+
+//go:generate easyjson $GOFILE
+
+//easyjson:json
+type SubscriptionDTO struct {
+	ServiceName string `json:"service_name"`
+	Price       int    `json:"price"`
+	UserID      string `json:"user_id"`
+	StartDate   string `json:"start_date"`
+	EndDate     string `json:"end_date,omitempty"`
+}
+
+//easyjson:json
+type CreateSubscriptionRequest struct {
+	ServiceName string `json:"service_name"`
+	Price       int    `json:"price"`
+	UserID      string `json:"user_id"`
+	StartDate   string `json:"start_date"`
+	EndDate     string `json:"end_date,omitempty"`
+}
+
+//easyjson:json
+type CreateSubscriptionResponse struct {
+	SubscriptionID string `json:"subscription_id"`
+}
+
+//easyjson:json
+type ReadSubscriptionResponse struct {
+	ServiceName string `json:"service_name"`
+	Price       int    `json:"price"`
+	UserID      string `json:"user_id"`
+	StartDate   string `json:"start_date"`
+	EndDate     string `json:"end_date,omitempty"`
+}
+
+//easyjson:json
+type UpdateSubscriptionRequest struct {
+	ServiceName string `json:"service_name"`
+	Price       int    `json:"price"`
+	UserID      string `json:"user_id"`
+	StartDate   string `json:"start_date"`
+	EndDate     string `json:"end_date,omitempty"`
+}
+
+//easyjson:json
+type ListSubscriptionsResponse struct {
+	Subscriptions []SubscriptionDTO `json:"subscriptions"`
+}
+
+//easyjson:json
+type CalcTotalCostResponse struct {
+	TotalCost int `json:"total_cost"`
+}
